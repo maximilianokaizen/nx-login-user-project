3805f020f2c446280b987536a45e020a7115729f
Maximiliano Rossi <148482605+maximilianokaizen@users.noreply.github.com>
2024-02-21 16:29:47 -0300

    added control to redirect

--- loginapp/panel/src/app/components/profile/profile.tsx
+++ loginapp/panel/src/app/components/profile/profile.tsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Button, Typography, Container } from '@mui/material';
 
 const Profile = () => {
+  useEffect(() => {
+    const userData = sessionStorage.getItem('user');
+    if (!userData) {
+      window.location.replace('/'); // Redirigir a la página de inicio
+    }
+  }, []);
+
+  const handleLogout = () => {
+    sessionStorage.removeItem('user');
+    window.location.replace('/');
+  };
+
   return (
     <Container maxWidth="xs">
       <Typography variant="h4" component="h1" gutterBottom>
         Profile
       </Typography>
+      <Button onClick={handleLogout} variant="contained" color="primary">
+        Logout
+      </Button>
     </Container>
   );
 };
--- loginapp/panel/src/main.tsx
+++ loginapp/panel/src/main.tsx
@@ -2,13 +2,16 @@ import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './app/app';
+import { AuthProvider } from './AuthContext';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthProvider>
+         <App />
+      </AuthProvider>
     </BrowserRouter>
   </StrictMode>
 );
