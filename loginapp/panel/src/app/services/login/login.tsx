779a91aa76a788f7f035052123f1ffb010c787b0
Maximiliano Rossi <148482605+maximilianokaizen@users.noreply.github.com>
2024-02-21 11:47:37 -0300

    added service

---
+++ loginapp/panel/src/app/services/login/login.tsx
@@ -0,0 +1,16 @@
+export const login = async (email : string, password : string) => {
+    const response = await fetch('http://localhost:3000/api/users/auth', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, password }),
+    });
+    if (response.ok) {
+      const data = await response.json();
+      console.log('data', data);
+      console.log('Login successful:', data);
+    } else {
+      console.error('Login failed:', response.statusText);
+    }
+  };
