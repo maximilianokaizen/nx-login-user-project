38f3886e6d3194c486197b7abbbb30c5fb24b3fa
Maximiliano Rossi <148482605+maximilianokaizen@users.noreply.github.com>
2024-02-22 16:15:56 -0300

    added decorators to health endpoint

--- loginapp/ms-users-login/src/health/health.controller.ts
+++ loginapp/ms-users-login/src/health/health.controller.ts
@@ -1,8 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @Controller('health')
+@ApiTags('Health')
 export class HealthController {
   @Get('')
+  @ApiOperation({ summary: 'Check health status' })
+  @ApiResponse({ status: 200, description: 'Returns the health status', type: Object })
   checkHealth() {
     return { status: 'ok' };
   }
